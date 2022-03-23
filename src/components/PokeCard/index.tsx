09b4bc9d517a2f24e3c13ac0a88556c82f9cde1e
Bruno Alves <hello@brunoalves.app>
2022-03-23 13:10:15 +0000

    click in pokemon card it is removed

--- src/components/PokeCard/index.tsx
+++ src/components/PokeCard/index.tsx
@@ -1,6 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
-
 import { PokemonNextApiResponse } from '../../types/pokemon'
 
 import { Container } from './styles'
@@ -8,9 +6,9 @@ interface PokeCardProps {
 	pokemon: PokemonNextApiResponse
 }
 
-function PokeCard({ pokemon, ...props }: PokeCardProps) {
+function PokeCard({ pokemon }: PokeCardProps) {
 	return (
-		<Link href={pokemon.name}>
+		<>
 			<Container
 				style={{
 					backgroundColor: `${pokemon.bgColor.replace(')', ', 0.45)')}`,
@@ -27,7 +25,7 @@ function PokeCard({ pokemon, ...props }: PokeCardProps) {
 				/>
 				<p className="pokemon-name">{pokemon.name}</p>
 			</Container>
-		</Link>
+		</>
 	)
 }
 
