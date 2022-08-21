3982ab8d78c993553c19225c8e68fbe62ad0bd01
Bruno Alves <hello@brunoalves.app>
2022-08-21 19:12:12 +0100

    api/pokemons refactor

--- src/pages/api/pokemons.ts
+++ src/pages/api/pokemons.ts
@@ -1,24 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getPokemonInfo } from 'src/lib/get-pokemon-info'
 import { loadPokemons } from 'src/lib/load-pokemon'
-import { NamedAPIResource, Pokemon } from 'src/types'
+import { NamedAPIResource } from 'src/types'
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   try {
     const data: NamedAPIResource[] = await loadPokemons()
 
-    const pokemons: Pokemon[] = await Promise.all(data.map(async (pokemon) => {
-      const pokemonInfo = await fetch(pokemon.url)
-      const result = await pokemonInfo.json()
-      return result
+    const pokemons = await Promise.all(data.map(async (pokemon) => {
+      const info = await getPokemonInfo(pokemon.name)
+      return info
     }))
 
     res.status(200)
-    res.json(
-      {
-        pokemons
-      }
-    )
+    res.json({ pokemons })
 
   } catch (err) {
     res.status(500)
