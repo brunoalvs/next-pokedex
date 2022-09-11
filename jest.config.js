44c007217f74df497cca5ee75d8be138a160477b
Bruno Alves <hello@brunoalves.app>
2022-09-11 01:58:34 +0100

    path settings

--- jest.config.js
+++ jest.config.js
@@ -18,9 +18,11 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   moduleNameMapper: {
     '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/mock/(.*)$': '<rootDir>/src/tests/mock/$1',
     '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
     '^@/templates/(.*)$': '<rootDir>/src/templates/$1',
     '^@/tests/(.*)$': '<rootDir>/src/tests/$1',
+    '^@/types/(.*)$': '<rootDir>/src/types/$1',
     '^@/utils/(.*)$': '<rootDir>/src/utils/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
