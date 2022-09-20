49bdf91f66f8d0559f3eecc09cdcae59a9981cef
Bruno Alves <hello@brunoalves.app>
2022-09-20 21:43:38 +0100

    all fetchs in pokeapi now use fetchPokemons function

--- src/utils/get-pokemon-infocard.ts
+++ src/utils/get-pokemon-infocard.ts
@@ -1,10 +1,10 @@
 import { GetPokemonInfoToCardResponse } from '@/types/default'
+import { fetchPokemons } from './fetch-pokemons'
 
 export const getPokemonInfoToCard = async (
   name: string
 ): Promise<GetPokemonInfoToCardResponse> => {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-  const data = await res.json()
+  const data = await fetchPokemons(`https://pokeapi.co/api/v2/pokemon/${name}`)
 
   const result: GetPokemonInfoToCardResponse = {
     id: data.id,
--- src/utils/get-pokemons.ts
+++ src/utils/get-pokemons.ts
@@ -1,8 +1,7 @@
 import { GetPokemonsResponse } from '@/types/default'
+import { fetchPokemons } from './fetch-pokemons'
 
 export const getPokemons = async (): Promise<GetPokemonsResponse> => {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/')
-  const data: GetPokemonsResponse = await res.json()
-
-  return data
+  const res: GetPokemonsResponse = await fetchPokemons('https://pokeapi.co/api/v2/pokemon/')
+  return res
 }
