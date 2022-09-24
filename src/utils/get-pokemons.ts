628ddf3ad82a579de0bdfdee518ab8ff0fd85af7
Bruno Alves <hello@brunoalves.app>
2022-09-24 18:29:20 +0100

    pokeLenght now have a default value

--- src/utils/get-pokemons.ts
+++ src/utils/get-pokemons.ts
@@ -2,7 +2,7 @@ import { GetPokemonsResponse } from '@/types/default'
 import { fetchPokemons } from './fetch-pokemons'
 
 export const getPokemons = async (
-  pokeLenght: number
+  pokeLenght = 0
 ): Promise<GetPokemonsResponse> => {
   const res: GetPokemonsResponse = await fetchPokemons(
     `https://pokeapi.co/api/v2/pokemon/?offset=${pokeLenght}&limit=${
