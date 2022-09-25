36dd12f0556eaecffd5ed718a7e60cb2e19c0f67
Bruno Alves <hello@brunoalves.app>
2022-09-25 18:33:19 +0100

    feat: PokemonName styled

--- src/components/PokemonName/styles.ts
+++ src/components/PokemonName/styles.ts
@@ -1,7 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.section``;
+export const Container = styled.section`
+  border: 1px dashed hotpink;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-export const JapaneseName = styled.h2``;
+export const JapaneseName = styled.h2`
+  font-size: 6rem;
+  font-weight: bold;
+  color: ${({ theme }) => theme.shape};
+  position: absolute;
+`;
 
-export const EnglishName = styled.h1``;
+export const EnglishName = styled.h1`
+  font-size: 4rem;
+  color: ${({ theme }) => theme.text};
+  isolation: isolate;
+`;
