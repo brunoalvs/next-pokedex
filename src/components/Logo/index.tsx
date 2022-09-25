dde3401ef9b387121ca98e70d7108fb635841483
Bruno Alves <hello@brunoalves.app>
2022-09-25 02:15:00 +0100

    Logo component added

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './styles'
+
+export const Logo = () => {
+  return (
+    <Container>
+      Pokédex
+    </Container>
+  )
+}
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.h2`
+  font-size: 1.5rem;
+  color: #2e3057; //var(--text-title);
+`;
