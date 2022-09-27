11651ca13ed310be9a66443f890653e788531cc6
Bruno Alves <hello@brunoalves.app>
2022-09-27 12:06:44 +0100

    Logo now go to back

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,10 +1,17 @@
 import styled from 'styled-components';
 
 export const Container = styled.h2`
+  color: #2e3057; //var(--text-title);
+  display: inline;
   font-size: 1.5rem;
   font-family: 'Poppins', sans-serif;
   font-weight: 700;
-  color: #2e3057; //var(--text-title);
+  transition: filter 0.2s ease;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(1.3);
+  }
 
   @media (min-width: 768px) {
     font-size: 2.25rem;
